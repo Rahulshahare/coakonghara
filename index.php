36a06f9bd99c10bc7a62f9b0970e4a31495c1c82
rahul shahare <rahulshahare@gmail.com>
2018-06-15 22:03:28 +0530

    working

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
+    <title>Collage Of Agriculture Konghara</title>
+    <link rel="icon" href="images/fevicon.ico" type="image/x-icon">
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/user.css" rel="stylesheet">
+    
   </head>
   <body>
     <!--Navigation-->
@@ -21,38 +24,36 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">Brand</a>
+      <a class="navbar-brand" href="#" title="Collage Of Agriculture Konghara">COAKONGHARA</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">About</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Facilities <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Library</a></li>
             <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="#">Labortory</a></li>
             <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
+            <li><a href="#">Campus</a></li>
           </ul>
         </li>
       </ul>
      
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
+        <li><a href="#">Contact us</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">its Menu <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Menu1</a></li>
+            <li><a href="#">Menu2</a></li>
+            <li><a href="#">Menu3</a></li>
             <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="#">Menu4</a></li>
           </ul>
         </li>
       </ul>
@@ -60,10 +61,125 @@
   </div><!-- /.container-fluid -->
 </nav>
     <!--Navigation-->
+    <div class="container-fluid slider">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+  </ol>
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="./images/val.jpg" alt="...">
+      <div class="carousel-caption">
+        <h4>Oceangreen Technology</h4>
+        <p>We Work For Excellence</p>
+      </div>
+    </div>
+    <div class="item">
+      <img src="./images/val.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="./images/val.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="./images/val.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="./images/val.jpg" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+    </div>
+    </div>
+
+    <div class="container-fluid nextStep stats">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <span class="text-center">
+                <h1>Oceangreen Technology</h1>
+                <h3>We work for excellence</h3>
+                </span><br/>
+                <div class="row">
+                        <div class="col-md-4 post">
+                            <img src="./images/val.jpg" width="100%"/>
+                            <br/><br/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                d do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation .
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                            </p>
+                        </div>
+                        <div class="col-md-4 post">
+                            <img src="./images/val.jpg" width="100%"/>
+                            <br/><br/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                d do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation .
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                            </p>
+                        </div>
+                        <div class="col-md-4 post">
+                            <img src="./images/val.jpg" width="100%"/>
+                            <br/><br/>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                d do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                                Ut enim ad minim veniam, quis nostrud exercitation .
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                            </p>
+                        </div>
+                        
+                                              
+                </div>
+            </div>
+        </div>
+    </div>
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery-2.1.1.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $('.carousel').carousel('cycle',function() {
+            interval: 100
+            })
+    </script>
   </body>
 </html>
